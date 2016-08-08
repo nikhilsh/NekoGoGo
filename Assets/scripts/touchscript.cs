ba091b9e7d6b9d3fed4fc34fff9d052d519afd05
Nikhil Sharma <me@nikhil.sh>
2016-08-08 23:25:18 +0800

    touch update stuff

--- Assets/scripts/touchscript.cs
+++ Assets/scripts/touchscript.cs
@@ -2,25 +2,18 @@
 using System.Collections;
 
 public class touchscript : MonoBehaviour {
+	public int index;
 	private Vector3 mousePosition;
 	float moveSpeed = 10.0f;
 
 	void Update () {            
 		for (var i = 0; i < Input.touchCount; ++i) {
-			Touch touch = Input.GetTouch(i);
-			if (touch.phase == TouchPhase.Began) {
-				if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
-					mousePosition = touch.position;
-					mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-					transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-				}
+			Touch touch = Input.GetTouch(index);
+			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+				mousePosition = touch.position;
+				mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+				transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
 			}
-		}
-
-		if (Input.GetMouseButton(0)) {
-			mousePosition = Input.mousePosition;
-			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-			transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
 		}
 	}
 }
