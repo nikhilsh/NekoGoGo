219cac191aef6e0cdb95eb88e4fac91794e24f0c
AceFlameAcer <ace.flame.acer@gmail.com>
2016-08-09 14:52:30 +0800

    JK Side

---
+++ Assets/scripts/StartMenu_Animation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary 
+{
+	public Vector3 cloud_3_posMin, cloud_3_posMax;
+}
+
+public class StartMenu_Animation : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+	public GameObject cloud_3;
+
+	void Update () {
+	}
+}
---
+++ Assets/scripts/StartMenu_Button.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu_Button : MonoBehaviour {
+
+	public void startGame() {
+		Application.LoadLevel ("test");
+	}
+
+
+	void Update () {
+	}
+}
