ad682db51ad828a242fe800c06dbe9e406eca85a
AceFlameAcer <ace.flame.acer@gmail.com>
2016-08-09 23:33:51 +0800

    Music, SFX, Art added

--- Assets/scripts/StartMenu_Animation.cs
+++ Assets/scripts/StartMenu_Animation.cs
@@ -5,14 +5,65 @@ using System.Collections;
 public class Boundary 
 {
 	public Vector3 cloud_3_posMin, cloud_3_posMax;
+	public Vector3 cloud_2_posMin, cloud_2_posMax;
+	public Vector3 cloud_1_posMin, cloud_1_posMax;
 }
 
 public class StartMenu_Animation : MonoBehaviour {
+	
+	private bool cloud_3_dirRight = true;
+	private bool cloud_2_dirRight = true;
+	private bool cloud_1_dirRight = true;
 
 	public float speed;
 	public Boundary boundary;
 	public GameObject cloud_3;
+	public GameObject cloud_2;
+	public GameObject cloud_1;
 
 	void Update () {
+		Debug.Log (cloud_1.transform.position.x);
+		animate_cloud_3 ();
+		animate_cloud_2 ();
+		animate_cloud_1 ();
+	}
+
+	private void animate_cloud_3() {
+		if (cloud_3_dirRight)
+			cloud_3.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		else
+			cloud_3.transform.Translate (-Vector3.right * speed * Time.deltaTime);
+		if (cloud_3.transform.position.x >= boundary.cloud_3_posMax.x) {
+			cloud_3_dirRight = false;
+		}
+		if (cloud_3.transform.position.x <= boundary.cloud_3_posMin.x) {
+			cloud_3_dirRight = true;
+		}
+	}
+
+	private void animate_cloud_2() {
+		if (cloud_2_dirRight)
+			cloud_2.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		else
+			cloud_2.transform.Translate (-Vector3.right * speed * Time.deltaTime);
+		if (cloud_2.transform.position.x >= boundary.cloud_2_posMax.x) {
+			cloud_2_dirRight = false;
+		}
+		if (cloud_2.transform.position.x <= boundary.cloud_2_posMin.x) {
+			cloud_2_dirRight = true;
+		}
+	}
+
+	private void animate_cloud_1() {
+		if (cloud_1_dirRight)
+			cloud_1.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		else
+			cloud_1.transform.Translate (-Vector3.right * speed * Time.deltaTime);
+		if (cloud_1.transform.position.x >= boundary.cloud_1_posMax.x) {
+			cloud_1_dirRight = false;
+		}
+		if (cloud_1.transform.position.x <= boundary.cloud_1_posMin.x) {
+			cloud_1_dirRight = true;
+		}
 	}
 }
--- Assets/scripts/StartMenu_Button.cs
+++ Assets/scripts/StartMenu_Button.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class StartMenu_Button : MonoBehaviour {
 
+	public AudioClip click;
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+
 	public void startGame() {
-		Application.LoadLevel ("test");
+		audio.PlayOneShot (click, 2.0f);
+		Application.LoadLevel ("DUMMY_GameStart");
 	}
-
-
-	void Update () {
-	}
+		
 }
