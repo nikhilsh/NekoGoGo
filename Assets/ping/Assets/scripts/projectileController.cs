b1cae90aca10afde52bb0d3a09a0ae4623270988
Nikhil Sharma <me@nikhil.sh>
2016-08-11 10:49:08 +0800

    try to fix cat

---
+++ Assets/ping/Assets/scripts/projectileController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileController : MonoBehaviour {
+	public float moveSpeed;
+
+	public GameObject smoke;
+	GameObject scoremanager;
+	ScoreManager _scoremanager;
+
+	public AudioClip meowing;
+
+
+	private float smokeTimer=0.0f;
+	public float smokeTime;
+	public float offset = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		scoremanager = GameObject.FindWithTag ("ScoreManager");
+		_scoremanager = scoremanager.GetComponent<ScoreManager>();
+
+		smokeTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		smokeTimer += Time.deltaTime;
+		if (smokeTimer > smokeTime) {
+			
+			
+			Instantiate(smoke, new Vector3(transform.position.x + offset,transform.position.y, transform.position.z ), transform.rotation);
+			smokeTimer = 0.0f;
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+	}
+
+	void FixedUpdate(){
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "CatController")
+		{
+			AudioSource.PlayClipAtPoint(meowing, transform.position);  
+			Destroy (this.gameObject);
+			_scoremanager.addScore (5);
+
+
+		}
+	}
+	
+}
---
+++ Assets/ping/Assets/scripts/shooterController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooterController : MonoBehaviour {
+	public GameObject spawnObject;
+	public float maxTime = 5;
+	public float minTime = 2;
+
+	private float time;
+	private float spawnTime;
+
+	public AudioClip flySound;
+
+	GameObject scoremanager;
+	ScoreManager _scoremanager;
+
+	// Use this for initialization
+	void Start () {
+		setRandomTime ();
+		time = minTime;
+
+		scoremanager = GameObject.FindWithTag ("ScoreManager");
+		_scoremanager = scoremanager.GetComponent<ScoreManager>();
+	}
+
+	void FixedUpdate(){
+		if (_scoremanager.initialized) {
+			time += Time.deltaTime;
+			if (time >= spawnTime) {
+				SpawnObject ();
+				setRandomTime ();
+			}
+		}
+	}
+	
+	void SpawnObject(){
+		time = 0;
+		Instantiate (spawnObject, transform.position, spawnObject.transform.rotation);
+		AudioSource.PlayClipAtPoint(flySound, transform.position);  
+
+	}
+
+	void setRandomTime(){
+		spawnTime = Random.Range (minTime, maxTime);
+	}
+}
